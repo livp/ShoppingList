4633d6e6dad0bc37f3e7feff84e406364dda829b
Livia <liv.pinera@gmail.com>
2023-01-12 16:36:17 -0800

    Read file at the start of the program and save it during exit

--- shopping_list.py
+++ shopping_list.py
@@ -7,18 +7,16 @@ import json
 #   shopping_list = { "Apple": { "isle": 1, "quantity": 5}, "Pineapple": { "isle": 3, "quantity": 15 } }
 #
 
-shopping_list = {}  # The variable for our shopping cart. Here's where all the data lives.
 
-
-def add_product(name, quantity, isle):
+def add_product(shopping_list, name, quantity, isle):
     shopping_list[name] = {"quantity":quantity, "isle":isle}
 
 
-def remove_product(name):
+def remove_product(shopping_list, name):
     del shopping_list[name]
 
 
-def modify_product(name, quantity, isle=0):
+def modify_product(shopping_list, name, quantity, isle=0):
     "If the parameter isle is not specified or 0, the isle will not be modified."
     if name in shopping_list.keys():
         # Modify an existing product
@@ -28,39 +26,39 @@ def modify_product(name, quantity, isle=0):
     else:
         add_product(name, quantity, isle)
 
-def load_data(filename)
+def load_data(filename):
     with open(filename) as input_file:
-        shopping_list = json.load(input_file)
+        return json.load(input_file)
 
 
-def save_data(filename)
+def save_data(shopping_list, filename):
     with open(filename, "w") as output_file:
         json.dump(shopping_list, output_file, indent=4)
 
 
 if __name__ == '__main__':
     # Create a prompt loop with the main menu
-    # load_data("shopping_list.json")
+    shopping_list = load_data("shopping_list.json")
     while True:
         match(int(input("Enter 1 to Add, 2 to Remove, 3 to Modify, 4 to List, 5 to Save and Exit: "))):
             case 1:
                 name = input("Please enter name of product: ")
                 quantity = int(input("Enter quantity needed: "))
                 isle = int(input("Enter isle where product is located: "))
-                add_product(name, quantity, isle)
+                add_product(shopping_list, name, quantity, isle)
             case 2:
                 name = input("Please enter name of product: ")
-                remove_product(name)
+                remove_product(shopping_list, name)
             case 3:
                 name = input("Please enter name of product: ")
                 quantity = int(input("Enter quantity needed: "))
                 # ask if isle has changed here?
                 isle = int(input("Enter isle where product is located, or enter 0 if not changed: "))
-                modify_product(name, quantity, isle)
+                modify_product(shopping_list, name, quantity, isle)
             case 4:
                 print(shopping_list)
             case 5:
-                save_data("shopping_list.json")
+                save_data(shopping_list, "shopping_list.json")
                 print ("Thank you and goodbye!")
                 break
         print ("\n") # newline
